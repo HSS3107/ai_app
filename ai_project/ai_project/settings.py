92361172bbce8a54f402faffa84ff3e5da3b1c92
drolkrad <drolkrad@vmi2130609.contaboserver.net>
2024-09-22 18:52:22 +0530

    added allowed hosts

--- ai_project/ai_project/settings.py
+++ ai_project/ai_project/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-oxchz10h0zzb@v!9*c*1)*v0%(#_7==67xp^6a%0#sf2mor2)#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','diveinyt.com','www.diveinyt.com']
 
 
 # Application definition
