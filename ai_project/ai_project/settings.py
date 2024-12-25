ae6ccb1ff8ce6303216818c2f8710b9dd76978f5
Harshit Singh Shishodia <hss31071994@gmail.com>
2024-12-25 14:31:12 +0530

    backend fix

--- ai_project/ai_project/settings.py
+++ ai_project/ai_project/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-import dj_database_url
 import logging.config
 
 
@@ -94,9 +93,14 @@ WSGI_APPLICATION = 'ai_project.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default= os.environ.get('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'Harshit',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
 
 
